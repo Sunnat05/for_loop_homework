360b310132ddc353696249524ee3352080edfb68
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-08-01 18:34:17 +0500

    nth

--- For04.py
+++ For04.py
@@ -1,11 +1,3 @@
 def main(A,B):
-    """
-    Return the numbers from A to B in the form of a list.
-    Args:
-        A: int
-        B: int
-    Returns:
-        list: return  answer
-    """
-    return list(range(A,B))
-print(main(3,9))
+       return list(range(A,B))
+print(main(3,7))
