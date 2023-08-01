26f93ad5cce52cfd9d4775632f3cafcc947b731e
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-08-01 19:09:10 +0500

    nth

--- For09.py
+++ For09.py
@@ -1,4 +1,6 @@
 def main(price):
+    for a in list(range(1,11)):
+        b=a*price
     """
     The price of a kilogram of sweets is given. Return the price of a dessert from one to ten kg in the form of a list.
     Args:
@@ -6,4 +8,5 @@ def main(price):
     Returns:
         list: return  answer
     """
-    return
+    return list(range(price,b+1,price))
+print(main(5))
