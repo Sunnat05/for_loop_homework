d3a4dbdcad5281f65be58a98e479c5ddbd85ea8e
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-09-06 12:53:22 +0500

    nth

--- For10.py
+++ For10.py
@@ -1,9 +1,14 @@
 def main(list1):
-    """
+    for a in list(range(list1)):
+        a=a
+        if a in "QWERTYUIOPLKJHGFDSAZXCVBNM": 
+            return a        
+            """
     Returns a list where only the first letter of each name is capitalized.
     Args:
         list1: list
     Returns:
         list: return  answer
     """
-    return
+    return list(range(a))
+print(main(ASGHJsdgh))
